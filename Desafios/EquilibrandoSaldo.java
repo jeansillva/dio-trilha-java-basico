8a35fbed32dbb4b710cc93b967beb082c95edf27
Jean Carlos <jeancarlos_bh@outlook.com>
2023-09-07 17:23:31 -0300

    Create EquilibrandoSaldo.java

---
+++ Desafios/EquilibrandoSaldo.java
@@ -0,0 +1,26 @@
+package Desafios;
+
+import java.util.Scanner;
+
+public class EquilibrandoSaldo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // o programa deve solicitar uma entrada que representa o saldo atual do
+        // funcionário
+        System.out.println("Olá! Seja bem vindo! "
+                + "Digite seu saldo atual:");
+        double saldoAtual = sc.nextDouble();
+
+        // O programa deve atualizar o saldo com base nas transações e exibir o saldo
+        // final.
+        System.out.println("Digite o valor para depósito: ");
+        double valorDeposito = sc.nextDouble();
+        System.out.println("Digite o valor do saque: ");
+        double valorRetirada = sc.nextDouble();
+
+        double saldoAtualizado = saldoAtual + valorDeposito - valorRetirada;
+
+        // TODO: Imprimir o a saída de conforme a tabela de exemplos (uma casa decimal).
+        System.out.printf("O valor do saldo atualizado é de: %.1f%n", saldoAtualizado);
+    }
+}
